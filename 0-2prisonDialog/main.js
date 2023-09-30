7e182d40376db41f4fed81d626ca6051c4ea5f77
wodus <wodustjdus34@gmail.com>
2023-10-01 00:03:01 +0900

    Fix 인코딩 문제 해결, 화면에 보임. 크기 조정 필요

--- 0-2prisonDialog/main.js
+++ 0-2prisonDialog/main.js
@@ -1,14 +1,12 @@
-//let postion = "middle";
-  let width = 900;
-  let height = 580;
-  App.onJoinPlayer.Add(function (player) {
-  //UI가 노출될 위치(정렬, 가로, 세로) 값을 변수로 사전에 제작
-  
+let _widget = null;
 
+App.onJoinPlayer.Add(function (player) {
   // my.html로 state라는 태그를 만들어 hello라는 값을 전달
-  let _widget = App.showWidget('my.html', "middle", width, height);
+    let _widget = App.showWidget('my.html', "top", 900, 800);
+  p._widget = App.showWidget('my.html', "top", 900, 800);
+  p.tag.widget = p.showWidget("widget.html", "sidebar", 350, 350);
+
 });
 
-//TryInvokeInternal():233 - Jint.Runtime.JavaScriptException: position is not defined at :9:3
-//모든 플레이어에게 지정된 align의 위치에 해당 html파일을 위젯으로 불러오기
-//App.showWidget(fileName: string, align: string, width: number, height: number): ScriptWidget
+//TryInvokeInternal():233 - Jint.Runtime.JavaScriptException: Cannot read property 'widget' of null at :6:14
+//TryInvokeInternal():233 - Jint.Runtime.JavaScriptException: p is not defined at :6:3
