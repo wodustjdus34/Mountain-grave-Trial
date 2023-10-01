8d7558c27453d906be3fbf2b80a99bd22fe5935c
wodus <wodustjdus34@gmail.com>
2023-10-01 16:28:20 +0900

    Style 0-4 dialog 이미지 추가

--- 0-4 yardDialog/main.js
+++ 0-4 yardDialog/main.js
@@ -7,7 +7,7 @@ App.onJoinPlayer.Add(function (player) {
       player.tag.widget.onMessage.Add(function (player, msg) {
           // 위젯에서 App으로 'type: close'라는 메시지를 보내면 위젯을 파괴함
           if (msg.type == "close") {
-              player.showCenterLabel("감옥을 탈출하자.");
+              player.showCenterLabel("이야기가 시작되는 곳으로 가자.");
               player.tag.widget.destroy();
               player.tag.widget = null;
           }
