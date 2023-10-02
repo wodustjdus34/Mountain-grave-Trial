86570a0c3d6cfabb301437e6cc3ff8679b30932b
wodus <wodustjdus34@gmail.com>
2023-10-03 03:27:25 +0900

    Feature 1-4 Dialog

--- 1-4 marketDialog/main.js
+++ 1-4 marketDialog/main.js
@@ -7,9 +7,11 @@ App.onJoinPlayer.Add(function (player) {
       player.tag.widget.onMessage.Add(function (player, msg) {
           // 위젯에서 App으로 'type: close'라는 메시지를 보내면 위젯을 파괴함
           if (msg.type == "close") {
-              //player.showCenterLabel("");
+              player.showCenterLabel("게시판에 진실을 밝히러 가자.");
               player.tag.widget.destroy();
               player.tag.widget = null;
           }
       });
+
+      
   });
