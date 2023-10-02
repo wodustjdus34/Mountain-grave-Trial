06833ca60b8bc71c63bc3d7ce5898beb160e49e5
wodus <wodustjdus34@gmail.com>
2023-10-03 02:17:59 +0900

    Chore 1-3 Dialog 이미지 삭제하여 씬 나누기

--- 1-3 storeDialog/main.js
+++ 1-3 storeDialog/main.js
@@ -10,10 +10,10 @@ App.onJoinPlayer.Add(function (player) {
         player.tag.widget.onMessage.Add(function (player, msg) {
           // 위젯에서 App으로 'type: close'라는 메시지를 보내면 위젯을 파괴함
             if (msg.type == "close") {
-              player.showCenterLabel("미니게임: (이대로는 계속 오해를 받겠어!) 조선에서 사용할 준호구를 발급해 보자");
+              player.showCenterLabel("(이대로는 계속 오해를 받겠어!) 조선에서 사용할 준호구를 발급해 보자.");
               player.tag.widget.destroy();
               player.tag.widget = null;
           }
       });
-	}, 15000);  
+	}, 5000);  
   });
