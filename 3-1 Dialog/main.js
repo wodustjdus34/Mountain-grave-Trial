f3e95c6dd87172fac271b18ce41a92135289e171
wodus <wodustjdus34@gmail.com>
2023-10-03 04:55:00 +0900

    Chore 대사 수정

--- 3-1 Dialog/main.js
+++ 3-1 Dialog/main.js
@@ -7,7 +7,7 @@ App.onJoinPlayer.Add(function (player) {
       player.tag.widget.onMessage.Add(function (player, msg) {
           // 위젯에서 App으로 'type: close'라는 메시지를 보내면 위젯을 파괴함
           if (msg.type == "close") {
-              player.showCenterLabel("3장: 고문서 콜렉터");
+              player.showCenterLabel("접대 장소는 우리 집을 나서면 금방 나올걸세.");
               player.tag.widget.destroy();
               player.tag.widget = null;
           }
