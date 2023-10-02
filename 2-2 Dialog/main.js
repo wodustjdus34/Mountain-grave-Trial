5f247186304d0f3cdab5036d5f14a8b2516c1bbc
wodus <wodustjdus34@gmail.com>
2023-10-03 04:47:33 +0900

    Feature 2-2 Dialog

--- 2-2 Dialog/main.js
+++ 2-2 Dialog/main.js
@@ -2,14 +2,16 @@ App.onJoinPlayer.Add(function (player) {
     player.tag = {
           widget: null,
       };
-  
-    player.tag.widget = player.showWidget("my.html", "top", 900, 800);
-      player.tag.widget.onMessage.Add(function (player, msg) {
+
+      setTimeout(function () {
+        player.tag.widget = player.showWidget("my.html", "top", 900, 800);
+        player.tag.widget.onMessage.Add(function (player, msg) {
           // 위젯에서 App으로 'type: close'라는 메시지를 보내면 위젯을 파괴함
-          if (msg.type == "close") {
-              //player.showCenterLabel("");
+            if (msg.type == "close") {
+              player.showCenterLabel("현감님께 이 사실을 알리러 가자.");
               player.tag.widget.destroy();
               player.tag.widget = null;
           }
       });
+	}, 17000); 
   });
