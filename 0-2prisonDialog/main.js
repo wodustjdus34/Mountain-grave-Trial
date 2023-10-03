498a47f7dfd181490e188332c5a9bdd1094aa36e
wodus <wodustjdus34@gmail.com>
2023-10-03 22:25:40 +0900

    Fix 대화창 10초 미루기

--- 0-2prisonDialog/main.js
+++ 0-2prisonDialog/main.js
@@ -3,15 +3,18 @@ App.onJoinPlayer.Add(function (player) {
 		widget: null,
 	};
 
-  player.tag.widget = player.showWidget("my.html", "top", 900, 800);
-	player.tag.widget.onMessage.Add(function (player, msg) {
-		// 위젯에서 App으로 'type: close'라는 메시지를 보내면 위젯을 파괴함
+	setTimeout(function () {
+		//App.sayToAll("5초후 메시지 출력");
+        player.tag.widget = player.showWidget("my.html", "top", 900, 800);
+        player.tag.widget.onMessage.Add(function (player, msg) {
+          // 위젯에서 App으로 'type: close'라는 메시지를 보내면 위젯을 파괴함
 		if (msg.type == "close") {
 			player.showCenterLabel("감옥을 살펴보며 기다리자.");
 			player.tag.widget.destroy();
 			player.tag.widget = null;
 		}
-	});
+      });
+	}, 10000);
 });
 
 //TryInvokeInternal():233 - Jint.Runtime.JavaScriptException: Cannot read property 'widget' of null at :6:14
