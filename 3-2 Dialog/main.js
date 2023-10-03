643bb79a2f7250ff1f297e0836bb3f9d9e65a05c
wodus <wodustjdus34@gmail.com>
2023-10-04 01:43:28 +0900

    Chore

--- 3-2 Dialog/main.js
+++ 3-2 Dialog/main.js
@@ -19,14 +19,17 @@ App.onJoinPlayer.Add(function(player){
     player.tag = {
           widget: null,
       };
-  
-    player.tag.widget = player.showWidget("my.html", "top", 900, 800);
-      player.tag.widget.onMessage.Add(function (player, msg) {
+    
+    setTimeout(function () {
+		//App.sayToAll("5초후 메시지 출력");
+        player.tag.widget = player.showWidget("my.html", "top", 900, 800);
+        player.tag.widget.onMessage.Add(function (player, msg) {
           // 위젯에서 App으로 'type: close'라는 메시지를 보내면 위젯을 파괴함
           if (msg.type == "close") {
-              //player.showCenterLabel("");
-              player.tag.widget.destroy();
-              player.tag.widget = null;
-          }
+            player.showCenterLabel("정자로 가보자!");
+            player.tag.widget.destroy();
+            player.tag.widget = null;
+        }
       });
+	}, 5000);
   });
